a167768ec3a8852f5aba47556126423d868ba05f
Emilio Martínez <emilio-mh@ciencias.unam.mx>
2018-02-23 20:31:58 -0600

    m

--- _build/recocido.ml
+++ _build/recocido.ml
@@ -1,146 +1,183 @@
 open Printf;;
 open Sqlite3;;
 
-let phi      = ref 0.8      in
-let temp_ini = ref 100.0    in
-let temp_min = ref 1.0      in
-let semilla  = ref 23       in
-let lote_tam = ref 100      in
+let semilla = ref 7          in
+let lote    = ref 25         in
+let temp_i  = ref 1.1        in
+let temp_m  = ref 0.001      in
+let phi     = ref 0.99       in
+let source  = ref "problema" in
+let n       = ref 1          in
 begin
 
+
 Arg.parse[
-  ("-phi", Arg.Float (function i -> phi      := i), "Factor de enfriamiento");
-  ("-ti" , Arg.Float (function i -> temp_ini := i), "Temperatura inicial"   );
-  ("-tm" , Arg.Float (function i -> temp_min := i), "Temperatura minima"    );
-  ("-s" ,  Arg.Int   (function i -> semilla  := i), "Semilla para el PRG"   );
-  ("-l" ,  Arg.Int   (function i -> lote_tam := i), "Tamaño del lote"       );
-  ](function s -> ()) "Sin parametros especificados" ;
+  ("-phi" , Arg.Float  (function i -> phi     := i), "Factor de enfriamiento"                 );
+  ("-ti"  , Arg.Float  (function i -> temp_i  := i), "Temperatura inicial"                    );
+  ("-tm"  , Arg.Float  (function i -> temp_m  := i), "Temperatura minima"                     );
+  ("-s"   , Arg.Int    (function i -> semilla := i), "Semilla para el PRG"                    );
+  ("-l"   , Arg.Int    (function i -> lote    := i), "Tamaño del lote"                        );
+  ("-src" , Arg.String (function i -> source  := i), "Archivo con la descripción del problema");
+  ("-n"   , Arg.Int    (function i -> n       := i), "Númro de evaluaciones"                  );
+  ](function s -> ()) "Error leyendo parámetros" ;
 
 Printf.printf "phi = %f\n" !phi;
-Printf.printf "t_i = %f\n" !temp_ini;
-Printf.printf "t_m = %f\n" !temp_min;
+Printf.printf "t_i = %f\n" !temp_i;
+Printf.printf "t_m = %f\n" !temp_m;
 Printf.printf "sem = %d\n" !semilla;
-Printf.printf "lot = %d\n" !lote_tam;
+Printf.printf "lot = %d\n" !lote;
+
+Random.init !semilla;
+
+let imprimir_arreglo out a = 
+for i = 0 to (Array.length a) - 1 do
+  fprintf out "%d " (a).(i)
+done;
+in
+
+let file = open_in !source in
+let lin = input_line file in
+close_in file;
+let re =  Str.regexp "[\r\n]" in
+let line = Str.global_replace re "" lin in
+
+let instancia = Array.of_list (List.map int_of_string (String.split_on_char ',' line)) in
+let n = Array.length instancia in
 
 
-let () = Random.init !semilla in
-
-let rec imprimir_lista l = match l with
-  | [] -> Printf.printf "\n"
-  | e :: t -> Printf.printf "%s " e; imprimir_lista t in
-let imprimir_sol s f =
-  Printf.printf "%f : " f;
-  for i = 0 to (Array.length s) - 1 do
-    Printf.printf "%d " (Array.get s i)
-  done;
-  Printf.printf "\n" in
-
-let file = open_in "problema" in
-let line = input_line file in
-let siz = String.length line in
-let liine = String.sub line 0 (siz-1) in
-let tmp = String.split_on_char ',' liine in
-let inst = Array.of_list (List.map int_of_string  tmp)in
-imprimir_sol inst 1.1 ;
-let n    = Array.length inst in
+(*Inicio de la lectura de la base de datos*)
 
 
 let db = db_open "tsp.db" in
+(*Leer el tamaño de la base de datos*)
+
 let e = ref 0 in
+let strr = (Array.fold_left (^) "" (Array.map ((^) ",") (Array.map string_of_int instancia)))in
+let str = String.sub strr 1 ((String.length strr) - 1) in
+
 exec db "SELECT count(id) FROM cities" ~cb:(fun row _ ->
   match row.(0) with
-  | Some a -> e := int_of_string a
-  | _ -> ());
-Printf.printf "%d\n" !e;
-let costos = Array.make_matrix (!e + 1) (!e + 1) 0.0 in
+    | Some a -> e := int_of_string a
+    | _ -> ());
+
+Printf.printf  "%s\n" str;
+
+let costos = Array.make_matrix (!e + 1) (!e + 1) 12982205.69 in
 let sum = ref 0.0 in
-let count = ref 0 in
-for i = 1 to !e do
-  for j = (i + 1) to !e do
-    let sql = sprintf "SELECT distance FROM connections WHERE id_city_1 = %d AND id_city_2 = %d" i j in
-    exec db sql ~cb:(fun row _ ->
-    match row.(0) with
-      | Some d ->
-        let w = float_of_string d in
-        Array.set costos.(i) j w;
-        Array.set costos.(j) i w;
+let e_s = ref 0   in
+
+(*Leer las distancias que se usarán*)
+begin
+  exec db (sprintf "SELECT * FROM connections WHERE id_city_1 IN (%s) AND id_city_2 IN (%s)" str str)
+    ~cb:(fun row _ ->
+    match row.(0), row.(1), row.(2) with
+      | Some a, Some b, Some c ->
+        let c1 = int_of_string   a in
+        let c2 = int_of_string   b in
+        let w  = float_of_string c in
+        Array.set costos.(c1) c2 w;
+        Array.set costos.(c2) c1 w;
         sum := !sum +. w;
-        count := !count + 1
-      | _->
-        let l1 = ref 0.0 in
-        let l2 = ref 0.0 in
-        let p1 = ref 0.0 in
-        let p2 = ref 0.0 in
-        (
-        exec db (sprintf "SELECT latitude, longitude FROM cities WHERE id = %d" i) ~cb:(fun row _ ->
-        match row.(0), row.(1) with
-          | Some lat, Some lon -> p1 := (float_of_string lat); l1 := (float_of_string lon); ()
-          | _, _ -> ());
-        exec db (sprintf "SELECT latitude, longitude FROM cities WHERE id = %d" j) ~cb:(fun row _ ->
-        match row.(0), row.(1) with
-          | Some lat, Some lon -> p2 := (float_of_string lat); l2 := (float_of_string lon); ()
-          | _, _ -> ()); ());
-          let w = 5.5 *. 6335.439 *. acos ( (sin !p1) *. (sin !p2) +. (cos !p1) *. (cos !p2) *. (cos (abs_float (!l1 -. !l2)))) in
-          Array.set costos.(i) j w;
-          Array.set costos.(j) i w)
-  done
-done;
-let prom = !sum /. (float_of_int !count) in
-
-let rand = (fun _ -> (fun _ -> (Random.int n) - 1)) in
+        e_s := !e_s + 1
+      | _ -> ());
+end;
 
 
+(*Fin de la lectura de la base de datos*)
 
-Array.sort rand inst;
+let promedio = !sum /. (float_of_int (!e_s - 1)) in
 
-
-let vecino x = 
-  let n = Array.length x in
-  (let i = Random.int n in
-  let j = Random.int n in
-  let aux = Array.get inst i in
-  Array.set inst i (Array.get inst j);
-  Array.set inst j aux;
-  x) in 
-
-let costo s =
+let costo (s : int array) : float = 
   let w = ref 0.0 in
   for i = 0 to (Array.length s) - 2 do
-    let tmp = costos.(Array.get s i).(Array.get s (i + 1)) in
-    w := !w +. tmp
-  done; !w /. (prom *. (float_of_int !e) -. 1.0) in
-
-
-let calculalote t s  =
-  let fs = costo !s in
-  let c = ref 0   in
-  let r = ref 0.0   in
-  let ss = ref (vecino !s) in
-  while !c < !lote_tam do
-    ss := vecino !s;
-    let fss = costo !ss in
-    if fss < fs +. t then (*Acepta nueva solución*)
-      imprimir_sol !s fss;
-      c := !c + 1;
-      s := !ss;
-      r := !r +. fss;
-      ()
+    w := !w +. (costos.(s.(i)).(s.(i+1)))
   done;
-  (!r /. float_of_int (!lote_tam), s) in
+  !w /. (promedio *. (float_of_int (!e_s + 1))) in
 
-let aceptacionumbrales t s = 
+let mejor_solucion = ref instancia in
+let mejor_costo = ref (costo instancia) in
+
+
+let vecino (s : int array ref) : (int array ref) =
+  let i = Random.int n in
+  let j = Random.int n in
+  let r = Array.copy !s in
+  Array.set r i (Array.get !s j);
+  Array.set r j (Array.get !s i);
+  ref r in
+
+let calcula_lote (t : float) (s : int array ref) : float*(int array) =
+  let c = ref 0   in
+  let r = ref 0.0 in
+  let i = ref 0   in
+  while !c < !lote && !i < !lote * !lote do(
+    let ss = vecino s in 
+    i := !i + 1;
+    let fs = costo !ss in
+    if fs < (costo !s) +. t then (
+        if fs < !mejor_costo then (
+          mejor_solucion := Array.copy !ss;
+          mejor_costo := fs;
+          printf "%f\n" fs;
+        );
+        s := !ss;
+        r := !r +. fs;
+        c := !c + 1;
+    );
+  )done;
+  ((!r /. (float_of_int !c)), !s) in
+
+let umbrales (t : float ref) (s : int array ref) : unit =
+  Array.sort (fun _ _ -> (Random.int 3) - 1) instancia;
+  printf "Inicio semilla %d\n" !semilla;
   let p = ref 0.0 in
-  while t > temp_min do
+  let i = ref 0 in
+  while !t > !temp_m do (
+    i := !i + 1;
     let q = ref Pervasives.max_float in
-    while !p < !q do
+    while !p < !q do(
       q := !p;
-      let (x, y) = calculalote !t s in
-      p := x;
-      s := !y;
-    done;
+      let (r, ns) = calcula_lote !t s in
+      p := r;
+      s := ns;
+    )done;
     t := !t *. !phi
-  done in
+  )done;
+  in
 
-aceptacionumbrales temp_ini (ref inst)
+let factible (s : int array) = 
+  let fact = ref true in
+  for i = 0 to (Array.length !mejor_solucion) - 2 do
+    let c1 = (Array.get !mejor_solucion i) in
+    let c2 = (Array.get !mejor_solucion (i + 1)) in
+    let sql = (sprintf "select count(distance) from connections where (id_city_1 = %d and id_city_2 = %d) or (id_city_1 = %d and id_city_2 = %d)" c1 c2 c2 c1) in 
+    match exec db sql ~cb:(fun r _ ->
+      match r.(0) with
+        | Some a ->  if 0 = (int_of_string a) then fact := false
+        | _ -> () ) with
+    | Rc.OK -> ()
+     | _ -> ()
+  done; !fact in
 
-end
+
+let rec eval (n : int) =
+  if n = 0 then
+    ()
+  else(
+    semilla := !semilla + n;
+    Random.init !semilla;
+    umbrales temp_i (ref instancia);
+    let out = open_out (sprintf "reporte%d.tsp" !semilla) in
+    imprimir_arreglo out !mejor_solucion;
+    fprintf out "\n%2.9f\n" !mejor_costo;
+    fprintf out "Fctible %B" (factible !mejor_solucion);
+    close_out out;
+    eval (n-1)
+  ) in
+
+let () = eval n in
+
+
+db_close db;
+()
+end; () 
