e794ec7d7f6d394e86659dbe64b190bbd4190cca
Emilio Martínez <emilio-mh@ciencias.unam.mx>
2018-03-23 23:56:41 -0600

    pdf

--- _build/evaluacion.ml
+++
@@ -1,8 +0,0 @@
-
-let costo (m : float array array) (a : float) (s : int array) : float = 
-  let w = ref 0.0 in
-  for i = 0 to (Array.length s) - 2 do
-    w := !w +. (m.(s.(i)).(s.(i+1)))
-  done;
-  !w /. (a *. (float_of_int (Array.length s - 1)) )
-
